387fdd622041353c01cf7d145421bc2b0d394cfe
Bill SJ Wu/WHQ/Wistron <bill_sj_wu@wistron.com>
2020-06-03 09:22:00 +0800

    remove duplication code from App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
 import React, {useState} from 'react';
 import {AppBar, IconButton, Toolbar, Typography} from '@material-ui/core';
 import {makeStyles} from '@material-ui/core/styles';
-import PropTypes from 'prop-types';
-import Box from "@material-ui/core/Box";
 import ManagementMenu from "./panel/ManagementMenu";
 import AwardListPanel from "./panel/AwardListPanel";
 import SearchPanel from "./panel/SearchPanel";
 import NormalMenu from "./panel/NormalMenu";
-
-function TabPanel(props) {
-    const {children, value, index, ...other} = props;
-
-    return (
-        <Typography
-            component="div"
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-        >
-            {value === index && <Box p={3}>{children}</Box>}
-        </Typography>
-    );
-}
-
-TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.any.isRequired,
-    value: PropTypes.any.isRequired,
-};
+import TabPanel from "./panel/TabPanel";
 
 const useStyles = makeStyles((theme) => ({
     root: {
--- src/panel/TabPanel.js
+++ src/panel/TabPanel.js
@@ -1,9 +1,16 @@
 import Box from "@material-ui/core/Box";
 import {Typography} from "@material-ui/core";
 import React from "react";
+// import PropTypes from "prop-types";
 
 export default (props) => {
     const {children, value, index, ...other} = props;
+
+    // const propTypes = {
+    //     children: PropTypes.node,
+    //     index: PropTypes.any.isRequired,
+    //     value: PropTypes.any.isRequired,
+    // };
 
     return (<Typography
         component="div"
