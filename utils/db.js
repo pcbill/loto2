0c91ce5de9f1d5895743035b30ade14f3c054d5c
Bill SJ Wu <bill_sj_wu@wistron.com>
2024-01-16 07:58:44 +0800

    update db info

--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,10 @@
 import pg from "pg";
 
 const client = new pg.Client({
-    user: '',
-    host: 'localhost',
-    database: 'postgres',
-    password: '',
+    user: 'npust',
+    host: 'dpg-ceopn5da4991ihh644p0-a',
+    database: 'mydb_qyjo',
+    password: 'da1SVS3RLaXI8LTmsrhZBFnOZRqa8xXA',
     port: 5432, // Default PostgreSQL port
 })
 
